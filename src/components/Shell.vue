4be562f806db7a5c53b38dbd45d43a51ba1fff99
Tyler Collier <git@tylercollier.com>
2020-12-12 00:06:10 -0700

    Start to make UI

---
+++ src/components/Shell.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-center">OpenReSync</h1>
+    <hr>
+    <div class="flex">
+      <div class="w-64 bg-gray-200">
+        <ul class="whitespace-no-wrap overflow-x-auto">
+          <li>Dashboard</li>
+          <li>ABOR Trestle</li>
+          <li>ABOR Bridge Interactive</li>
+          <li>here's an example of one that wraps</li>
+        </ul>
+      </div>
+      <div class="flex-1">
+        Here's the rest
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
