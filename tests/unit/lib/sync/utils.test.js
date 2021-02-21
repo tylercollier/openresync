7438b725a0325c64afe4f57d2b57779472c4066c
Tyler Collier <git@tylercollier.com>
2021-02-20 23:44:05 -0700

    Fixing $expand for purge was more involved than I thought, and downloader needed to be fixed too #1

--- tests/unit/lib/sync/utils.test.js
+++ tests/unit/lib/sync/utils.test.js
@@ -1,18 +1,5 @@
 const utils = require('../../../../lib/sync/utils')
 
-test('getOldestBatchId', () => {
-  const filesPerMlsResource = [
-    [
-      'sync_batch_2021-02-17-T-06-44-06-623Z_seq_2021-01-17-T-06-45-37-666Z.json',
-    ],
-    [
-      'sync_batch_2021-01-17-T-06-44-06-623Z_seq_2021-01-17-T-06-45-37-666Z.json',
-    ],
-  ]
-
-  expect(utils.getOldestBatchId(filesPerMlsResource, 'sync')).toEqual('2021-01-17-T-06-44-06-623Z')
-})
-
 describe('flattenExpandedMlsResources', () => {
   const mlsResources = [{
     name: 'Member',
@@ -30,11 +17,50 @@ describe('flattenExpandedMlsResources', () => {
   }]
 
   test('flattens and uniquifies', () => {
-    expect(utils.flattenExpandedMlsResources(mlsResources)).toEqual(expect.arrayContaining([
-      expect.objectContaining({ name: 'Member' }),
-      expect.objectContaining({ name: 'Office' }),
-      expect.objectContaining({ name: 'Property' }),
-      expect.objectContaining({ name: 'Media' }),
-    ]))
+    expect(utils.flattenExpandedMlsResources(mlsResources)).toEqual([{
+      name: 'Member',
+      expand: [{
+        name: 'Property',
+        expand: [{
+          name: 'Media',
+        }],
+      }],
+    }, {
+      name: 'Property',
+      expand: [{
+        name: 'Media',
+      }],
+    }, {
+      name: 'Media',
+    }, {
+      name: 'Office',
+      expand: [{
+        name: 'Property',
+      }],
+    }])
+  })
+})
+
+describe('getOldestBatchId', () => {
+  test('finds oldest timestamp', () => {
+    const filesPerMlsResource = [
+      [
+        'sync_batch_2021-02-17-T-06-44-06-623Z_seq_2021-01-17-T-06-45-37-666Z.json',
+      ],
+      [
+        'sync_batch_2021-01-17-T-06-44-06-623Z_seq_2021-01-17-T-06-45-37-666Z.json',
+      ],
+    ]
+
+    expect(utils.getOldestBatchId(filesPerMlsResource, 'sync')).toEqual('2021-01-17-T-06-44-06-623Z')
+  })
+
+  test('it returns null if no files are passed', () => {
+    expect(utils.getOldestBatchId([[]], 'sync')).toEqual(null)
+  })
+
+  test('it returns null if no files are matched', () => {
+    const filesPerMlsResource = [['purge_batch_2021-02-20-T-05-18-04-901Z_seq_2021-02-20-T-05-18-27-018Z.json']]
+    expect(utils.getOldestBatchId(filesPerMlsResource, 'sync')).toEqual(null)
   })
 })
