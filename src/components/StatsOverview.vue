d08f140c34e5d3f49eefaae2ac180a38f3392114
Tyler Collier <git@tylercollier.com>
2021-04-10 13:50:41 -0700

    Show purge data on dashboard when available #7

--- src/components/StatsOverview.vue
+++ src/components/StatsOverview.vue
@@ -8,17 +8,29 @@
         :title="sourceName"
         style="flex: 0 1 20rem;"
       >
-        <div v-if="s.sync.length">
-          <b-card-text>
-            <div>Last Batch: {{s.sync[0].batch_id}}</div>
+        <b-card-text>
+          <div v-if="s.sync.length">
+            <div>Last sync batch:<div class="tw-ml-4">{{ s.sync[0].batch_id }}</div></div>
             <div>Status: <span
               :class="{ 'text-success': s.sync[0].result === 'success', 'text-danger': s.sync[0].result === 'error' }">{{ s.sync[0].result }}</span>
             </div>
-          </b-card-text>
-        </div>
-        <div v-else>
-          <em>Never run</em>
-        </div>
+          </div>
+          <div v-else>
+            <em>Never synced</em>
+          </div>
+          <div v-if="s.purge.length && sourceName === 'aborTrestle'" class="tw-mt-2 tw-text-sm">
+            <div>Last purge batch:<div class="tw-ml-4">{{ s.purge[0].batch_id }}</div></div>
+            <div>Status: <span
+              :class="{ 'text-success': s.purge[0].result === 'success', 'text-danger': s.purge[0].result === 'error' }">{{ s.purge[0].result }}</span>
+            </div>
+          </div>
+          <!-- Here's a hack to add an equivalent amount of space if there is no purge data -->
+          <div v-else class="tw-invisible tw-mt-2 tw-text-sm">
+            <div>Last purge batch</div>
+            <div>Status:</div>
+            <div>-</div>
+          </div>
+        </b-card-text>
         <b-button class="tw-mt-4" variant="primary" :to="`/sources/${sourceName}`">View details</b-button>
       </b-card>
     </b-card-group>
