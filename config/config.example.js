685e3c6a722f354d35614e1fc254aec4de9da8e8
Tyler Collier <git@tylercollier.com>
2021-11-13 23:37:51 -0700

    Update example config to reflect config options in user config version 0.2.0

--- config/config.example.js
+++ config/config.example.js
@@ -4,7 +4,7 @@
 // source's getResourceEndpoint function later in the file.
 
 
-
+// All config values are required unless noted as optional.
 
 const pathLib = require('path')
 
@@ -19,8 +19,10 @@ module.exports = () => ({
   // resources. For convenience, you could share more of their configuration.
   sources: [
     {
-      // The name should be thought of as an ID. It will be used for directories and URLs, so don't spaces, or other characters not
-      // permitted in a URL. This name is arbitrary but must be unique among sources.
+      // This first example source uses Bridge Interactive. There is a second example source below that uses Trestle.
+
+      // The name should be thought of as an ID. It will be used for directories and URLs, so don't use spaces, or other
+      // characters not permitted in a URL. This name is arbitrary but must be unique among sources.
       name: 'aborBridgeInteractive',
 
       // Bridge Interactive only requires an access token, so we do not bother to specify a client ID or client secret
@@ -80,37 +82,6 @@ module.exports = () => ({
       // Bridge Interactive doesn't allow $orderby during replication.
       useOrderBy: false,
 
-      // destinations is an array of objects. Each object is a 'destination', or where data will be written to, such as
-      // a MySQL database.
-      destinations: [
-        {
-          // The name should be thought of as an ID, meant for computers. It is arbitrary but must be unique among
-          // destinations.
-          name: 'mysql1',
-
-          // The type of the destination, which is 'mysql' or 'devnull' (meant for debug purposes).
-          type: 'mysql',
-
-          // This is the config block specifically meant for the destination. It differs per destination,
-          // although it's moot now since only MySQL is supported.
-          config: {
-            // The username, password, host, port, database name all wrapped into one.
-            connectionString: 'mysql://user1:password1@localhost:33033/mymls_bridge',
-
-            // makeTableName allows you to change the name of the table used. It takes the name of the resource and
-            // you should return a string. An example use case is if you are using a single database for multiple MLS
-            // sources, you might not want each to use the name 'Property' for a table name, so you could use a prefix
-            // perhaps based on the name of the MLS, e.g. abor_Property (for Austin Board of Realtors) and
-            // crmls_Property (for California Regional MLS). This is optional and if not specified, the resource name
-            // will be used.
-            // makeTableName: name => 'mytableprefix_' + name,
-
-            // Same idea as makeTableName above
-            // makeFieldName: name => 'myfieldprefix_' + name,
-          },
-        },
-      ],
-
       // Specify the name of the platform. It's either 'bridgeInteractive' or 'trestle'. This will determine things like
       // how authenticatoin is handled, how the XML metadata is interpreted, which fields are ignored, etc.
       platformAdapterName: 'bridgeInteractive',
@@ -140,22 +111,174 @@ module.exports = () => ({
         },
       ],
 
-      // Here you can configure when this source and its resources will sync (or be purged). It is an object with two
-      // properties, sync and purge, which are both objects. Their only property is called cronString, and it is in the
+      // destinations is an array of objects. Each object is a 'destination', or where data will be written to, such as
+      // a MySQL database or Solr.
+      destinations: [
+        {
+          // Here's an example of a destination using the mysql data adapter.
+
+          // The type of the destination, which is 'mysql' or 'solr'.
+          type: 'mysql',
+
+          // The name should be thought of as an ID, meant for computers. It is arbitrary but must be unique among
+          // destinations.
+          name: 'mysql1',
+
+          // This is the config block specifically meant for the destination. It differs per destination. Here's an
+          // example for MySQL.
+          config: {
+            // The username, password, host, port, database name all wrapped into one.
+            connectionString: 'mysql://user1:password1@localhost:33033/mymls_bridge',
+
+            // Optional
+            // makeTableName allows you to change the name of the table used. It takes the name of the resource and
+            // you should return a string. An example use case is if you are using a single database for multiple MLS
+            // sources, you might not want each to use the name 'Property' for a table name, so you could use a prefix
+            // perhaps based on the name of the MLS, e.g. abor_Property (for Austin Board of Realtors) and
+            // crmls_Property (for California Regional MLS). This is optional and if not specified, the resource name
+            // will be used.
+            // makeTableName: name => 'mytableprefix_' + name,
+
+            // Optional
+            // makeFieldName allows you to change the name of the field used. RESO Web API field names are
+            // PascalCase, but you might prefer, for example, snake_case.
+            // Note: if you use the transform function, described below, the field names your function will receive are
+            // the keys from the object you return from transform().
+            // makeFieldName: name => 'myfieldprefix_' + name,
+
+            // Optional
+            // shouldSyncTableSchema allows you to opt out of a table's schema being synced. The MySQL data adapter
+            // synchronizes table schema, which is handy when new fields are added or removed
+            // over time. It takes the MLS resource name and should return a boolean. Default returns true.
+            // If you return false, it means that you are responsible for creating the table as well as altering.
+            // shouldSyncTableSchema: function(mlsResourceName) {
+            //   if (mlsResourceName === 'Media') {
+            //     return false
+            //   }
+            //   return true
+            // }
+
+            // Optional
+            // makeForeignKeyFieldName is used in the purge process. If, in the mlsResources section above, you use the
+            // 'expand' property to sync subresources, and the primary key of the subresource's table differs from the
+            // MLS's, you will need to use this. It's a function that takes in the parent MLS resource name, the sub-
+            // resource name, and the field name, and returns the name of your primary key.
+            // makeForeignKeyFieldName: (parentMlsResourceName, mlsResourceName, fieldName) => {
+            //   if (mlsResourceName === 'Media') {
+            //     if (parentMlsResourceName === 'Property') {
+            //       if (fieldName === 'ListingKey') {
+            //         return 'Content-ID'
+            //       }
+            //     }
+            //   }
+            //   return fieldName
+            // }
+
+            // Optional
+            // transform allows you to change the record of what would be inserted/updated in the database. If the only
+            // difference between what you want inserted/updated is the field names, then you should use the
+            // makeFieldName option. But this function would allow you to modify the data in any way, for example change
+            // keys, values, add key/value pairs, remove some, etc. It takes the MLS resource name, the record as
+            // downloaded, and the metadata object, and should return an object.
+            // Note: For the primary key's value, you may return null if your table's primary key is auto-incremented,
+            // which is the default.
+            // transform: (mlsResourceName, record, metadata) => {
+            //   // Return an object. Do not mutate record.
+            // }
+          },
+        },
+        {
+          // Here's an example of a destination using the Solr data adapter.
+
+          // The type of the destination, which is 'mysql' or 'solr'.
+          type: 'solr',
+
+          // The name should be thought of as an ID, meant for computers. It is arbitrary but must be unique among
+          // destinations.
+          name: 'solr1',
+
+          // Optional
+          // host: The host of the Solr instance.
+
+          // Optional
+          // port: The port of the Solr instance.
+
+          // Optional
+          // makeCoreName allows you to change the name of the core used. It takes the name of the resource and
+          // you should return a string. An example use case is if you are using a single Solr instance for multiple MLS
+          // sources, you might not want each to use the name 'Property' for a core name, so you could use a prefix
+          // perhaps based on the name of the MLS, e.g. abor_Property (for Austin Board of Realtors) and
+          // crmls_Property (for California Regional MLS). This is optional and if not specified, the resource name
+          // will be used.
+          // makeCoreName: name => 'mycoreprefix_' + name,
+
+          // FIXME: This option is not yet used in syncData().
+          // Optional
+          // makeFieldName allows you to change the name of the field used. RESO Web API field names are
+          // PascalCase, but you might prefer, for example, camelCase.
+          // Note: if you use the transform function, described below, the field names your function will receive are
+          // the keys from the object you return from transform().
+          // makeFieldName: name => 'myfieldprefix_' + name,
+
+          // Optional
+          // transform allows you to change the record of what would be inserted/updated in the core. If the only
+          // difference between what you want inserted/updated is the field names, then you should use the
+          // makeFieldName option. But this function would allow you to modify the data in any way, for example change
+          // keys, values, add key/value pairs, remove some, etc. It takes the MLS resource name, the record as
+          // downloaded, and the metadata object, a cache object (described below), and should return an object.
+          // The "cache" is an (originally an empty) object that we pass each time to the transform function. This
+          // allows the transform function to do lookup work when it chooses, e.g. it could do it all on the first pass
+          // and not again, or it could potentially do it only on-demand somehow.
+          // transform: (mlsResourceName, record, metadata, cache) => {
+          //   // Return an object. Do not mutate record.
+          // }
+        },
+      ],
+
+      // Optional
+      // Here you can configure when this source and its resources will sync, be purged, or be reconciled (see the
+      // README for the difference). It is an object with three properties, sync, purge, and reconcile, which are each
+      // objects. Their only property is called cronStrings, and it is an array of cron strings, in the
       // [normal cron format](https://www.npmjs.com/package/cron#cron-ranges). It is technically possible to specify
       // a cron schedule to run too often (like once per second), which should be avoided. A suggested cron schedule for
       // syncing would be `*/15 * * * *`, or every 15 minutes. Check in with your platform to know what's recommended.
       cron: {
+        // Optional. If not included, syncs will not be performed.
         sync: {
-          cronString: '0 * * * *',
+          // Optional
+          // Specify whether sync cron jobs will be run.
+          // enabled: true,
+
+          // Optional. If not included, syncs will not be performed.
+          // Specify an array of cron strings for when the sync cron job(s) should be run.
+          cronStrings: ['0 * * * *'],
         },
+
+        // Optional. If not included, purges will not be performed.
         purge: {
-          cronString: '30 0 * * *',
+          // Optional
+          // Specify whether purge cron jobs will be run.
+          // enabled: true,
+
+          // Optional. If not included, purges will not be performed.
+          // Specify an array of cron strings for when the purge cron job(s) should be run.
+          cronStrings: ['15 * * * *'],
+        },
+
+        // Optional. If not included, reconciles will not be performed.
+        reconcile: {
+          // Optional
+          // Specify whether reconcile cron jobs will be run.
+          // enabled: true,
+
+          // Optional. If not included, reconciles will not be performed.
+          // Specify an array of cron strings for when the reconcile cron job(s) should be run.
+          cronStrings: ['30 * * * *']
         },
       },
     },
 
-    // This second source is shown to contrast with the first. It is for Trestle as opposed to Brige Interactive.
+    // This second source is shown to contrast with the first. It is for Trestle as opposed to Bridge Interactive.
     // Not all values will be commented, just the ones that are different enough to explain.
     {
       name: 'aborTrestle',
@@ -219,17 +342,22 @@ module.exports = () => ({
       ],
       cron: {
         sync: {
-          cronString: '15 * * * *',
+          cronStrings: ['10 * * * *'],
         },
         purge: {
-          cronString: '45 0 * * *',
+          cronStrings: ['25 * * * *'],
+        },
+        reconcile: {
+          cronStrings: ['40 * * * *'],
         },
       },
     },
   ],
 
-  // The server runs on this port. Defaults to 4000.
+  // Optional
   server: {
+    // Optional
+    // The server runs on this port. Defaults to 4000.
     // port: 4000,
   },
 
