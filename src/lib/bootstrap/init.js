c94aad330a1bf8f5ea5c603c23915985c53bddcc
Tyler Collier <git@tylercollier.com>
2022-01-09 22:56:37 -0700

    Should've updated package-lock.json's version alongside package.json's version

--- src/lib/bootstrap/init.js
+++ src/lib/bootstrap/init.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 
-import { BootstrapVue, TableSimplePlugin, IconsPlugin } from 'bootstrap-vue'
+import {
+  BootstrapVue,
+  TableSimplePlugin,
+  IconsPlugin,
+  TooltipPlugin,
+} from 'bootstrap-vue'
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
+// Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './bootstrap-overrides.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.use(TableSimplePlugin)
+Vue.use(TooltipPlugin)
